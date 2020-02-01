893f7bcd9a66897a1a3828b72f5b3f3f1b5d8c26
matthewdadoun <matthewdadoun@gmail.com>
2020-02-01 18:52:15 -0500

    Added rerverse gun (main mechanic), particles, and ammo

--- Reverse Gun/Reverse Gun/Assets/Scripts/Hitscan.cs
+++ Reverse Gun/Reverse Gun/Assets/Scripts/Hitscan.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class Hitscan : MonoBehaviour
 {
     public Camera hitScanCam;
-
+    public ParticleSystem bullet;
+    public int ammo = 6;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && ammo > 0)
         {
             Hit();
+        }
+
+        if(Input.GetButtonDown("Fire2")&& ammo < 6)
+        {
+            Reverse();
         }
     }
 
@@ -21,7 +27,21 @@ public class Hitscan : MonoBehaviour
         if(Physics.Raycast(hitScanCam.transform.position, hitScanCam.transform.forward, out hit))
         {
             Debug.Log(hit.transform.name);
+            bullet.Emit(hitScanCam.transform.position, hitScanCam.transform.forward*100.0f, 1.0f, 1.0f, Color.red);
+            ammo--;
         }
 
+    }
+
+    void Reverse()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(hitScanCam.transform.position, hitScanCam.transform.forward, out hit))
+        {
+
+            bullet.Emit(hit.point, -hitScanCam.transform.forward * 100.0f, 1.0f, 1.0f, Color.red);
+            ammo++;
+        }
     }
 }
