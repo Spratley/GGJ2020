ab34d467a831156f58239469e5d29b006a1d1ee5
matthewdadoun <matthewdadoun@gmail.com>
2020-02-01 03:12:58 -0500

    Fixed collider, added jump, and hitscan

---
+++ Reverse Gun/Reverse Gun/Assets/Scripts/Hitscan.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitscan : MonoBehaviour
+{
+    public Camera hitScanCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Hit();
+        }
+    }
+
+    void Hit()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(hitScanCam.transform.position, hitScanCam.transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
--- Reverse Gun/Reverse Gun/Assets/Scripts/player_Movement.cs
+++ Reverse Gun/Reverse Gun/Assets/Scripts/player_Movement.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class player_Movement : MonoBehaviour
 {
-
+    public int jump;
     public CharacterController controller;
+    bool isJumping = false;
+    public float speed = 5f;
 
-    public float speed = 12f;
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpkey;
+
+    private void JumpInput()
+    {
+        if(Input.GetKeyDown(jumpkey) && !isJumping)
+        {
+            jump = 0;
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        float timeInAir = 0.0f;
+
+        do
+        {
+            jump++;
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            controller.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+            timeInAir += Time.deltaTime;
+            yield return null;
+        } while (controller.isGrounded == false && controller.collisionFlags != CollisionFlags.Above);
+
+        isJumping = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
+        JumpInput();
         Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
+        controller.SimpleMove(Vector3.ClampMagnitude(move, 1) * speed);
     }
 }
