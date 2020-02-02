09275009a74f6c0aa3e8ae657e19f4704a1e3121
Jacob Pratley <pratleyjacob@gmail.com>
2020-02-02 15:27:31 -0500

    Materials and game???

---
+++ Reverse Gun/Reverse Gun/Assets/Scripts/SceneSwitching.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    public void Close_game()//Closes the game
+    {
+        Application.Quit();
+    }
+
+    public void Load_Game() //Loads first level
+    {
+        SceneManager.LoadScene(sceneName: "SampleScene");
+    }
+}
