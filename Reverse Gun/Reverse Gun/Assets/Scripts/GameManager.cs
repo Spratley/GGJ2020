677ab0527e8147093a579fff9dc34d65bc7ddad1
Jacob Pratley <pratleyjacob@gmail.com>
2020-02-02 16:15:58 -0500

    Added working UI and win condition

---
+++ Reverse Gun/Reverse Gun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public Text good;
+    public Text bad;
+
+    public AICharacter[] goodBoyz;
+    public AICharacter[] badBoyz;
+
+    private void Start()
+    {
+        goodBoyz = GameObject.FindObjectsOfType<AICharacter>().Where(boy => boy.name.Contains("Ally")).ToArray();
+        badBoyz = GameObject.FindObjectsOfType<AICharacter>().Where(boy => boy.name.Contains("Enemy")).ToArray();
+    }
+
+    void Update()
+    {
+
+        AICharacter[] goodBoyz2 = goodBoyz.Where(boy => !boy.isDead).ToArray();
+        AICharacter[] badBoyz2 = badBoyz.Where(boy => boy.isDead).ToArray();
+
+        good.text = goodBoyz2.Count().ToString() + " / 21";
+        bad.text = badBoyz2.Count().ToString() + " / 21";
+
+        if (goodBoyz2.Count() == 21 && badBoyz2.Count() == 21)
+            Win();
+    }
+
+    void Win()
+    {
+        Application.Quit();
+    }
+}
